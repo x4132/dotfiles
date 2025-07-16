fa7e95415b94f5ce3262ea802b2c9a33327153d6
x4132 <31595285+x4132@users.noreply.github.com>
2025-07-15 22:21:29 -0700

    finished waybar config

--- scripts/executable_wallpapers.sh
+++ scripts/executable_wallpapers.sh
@@ -2,7 +2,7 @@ if ! pgrep -x "swww-daemon" > /dev/null; then
     swww-daemon &
 fi
 
-swww img -o "DP-1" ~/Pictures/wallpapers/jellyfish.jpg
-swww img -o "DP-2" ~/Pictures/wallpapers/hexmoon.jpg
+swww img -o "DP-1" ~/Pictures/wallpapers/great_wave_circle_minimal.jpg
+swww img -o "DP-2" ~/Pictures/wallpapers/wolf_cave_mountain_silhouette.png
 
 # kitty --start-as=hidden kitten panel --edge background --output-name "DP-2" --detach btop
